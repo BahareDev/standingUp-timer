3d7b7ef5d9f08d867889ac7d40a92d49016af6a3
BahareDev <bahare.taheri020@gmail.com>
2025-04-10 11:19:45 +0330

    inital files v1

---
+++ script.js
@@ -0,0 +1,70 @@
+"use strict";
+
+const startBtn = document.querySelector(".startBtn");
+const showTime = document.querySelector(".showTime");
+const infoTime = document.querySelector(".infoTime");
+const soundEffect = new Audio("./assets/ring-drop.mp3");
+
+function SessionHandler() {
+  const incBtn = document.querySelector(".inc");
+  const decBtn = document.querySelector(".dec");
+  const session = document.querySelector(".session");
+
+  let sessionValue = 45;
+  incBtn.addEventListener("click", () => {
+    sessionValue += 5;
+    session.textContent = sessionValue;
+  });
+
+  decBtn.addEventListener("click", () => {
+    sessionValue -= 5;
+    session.textContent = sessionValue;
+  });
+}
+
+function SetTimer(min = 45) {
+  // min to mls
+  let minValue = parseInt(min, 10) * 60000;
+
+  const End = document.querySelector(".EndBtn");
+
+  function updateTimer() {
+    const date = new Date();
+    date.setMinutes(date.getMinutes() + parseInt(min));
+    showTime.textContent = date.toLocaleTimeString();
+  }
+
+  updateTimer();
+
+  // timer starting
+  console.log("Staring...");
+
+  console.log(showTime);
+
+  let id = setInterval(() => {
+    soundEffect.play();
+    updateTimer();
+  }, minValue);
+
+  End.addEventListener(
+    "click",
+    () => {
+      clearInterval(id);
+      alert("stoped");
+      console.log("Stopd");
+    },
+    { once: true }
+  );
+}
+
+startBtn.addEventListener("click", () => {
+  let minuteValue = document.getElementById("insert-Min").textContent;
+
+  if (isNaN(minuteValue) || minuteValue <= 0) {
+    alert("Please enter a valid number greater than 0");
+  }
+  infoTime.textContent = `every ${minuteValue} minutes`;
+  SetTimer(minuteValue);
+});
+
+SessionHandler();
